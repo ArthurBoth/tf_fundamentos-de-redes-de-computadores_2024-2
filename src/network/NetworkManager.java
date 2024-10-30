a5a2034f18ee57bb804d9aa53c15757ee04dbc31
gru935 <j.sostruznik@edu.pucrs.br>
2024-10-30 18:19:13 -0300

    tentando inicializar o datagramsocket

--- src/network/NetworkManager.java
+++ src/network/NetworkManager.java
@@ -2,56 +2,27 @@ package network;
 
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.IOException;
+import java.net.DatagramPacket;
 
 import constants.ConfigurationConstants;
 import io.IOManager;
 
 public class NetworkManager implements Runnable {
-    private DatagramSocket socket;
+    private static DatagramSocket client;
     private static Scanner sc;
-    IOManager io;
-    ArrayList<Route> routes;
+    static IOManager io = new IOManager();
+    static ArrayList<Route> routes = new ArrayList<>();
 
-    public NetworkManager(DatagramSocket s) {
+    public NetworkManager() {
         io = new IOManager();
         routes = new ArrayList<>();
     }
 
-    public static void main(String[] args) throws SocketException, UnknownHostException {
-        if (args.length < 2) {
-            System.out.println("Usage: java Client <server_ip> <port>");
-            return;
-        }
-
-        String hostname = args[0];
-        int port = Integer.parseInt(args[1]);
-
-        sc = new Scanner(System.in);
-
-        DatagramSocket client = new DatagramSocket();
-
-        InetAddress ipAddress = InetAddress.getByName(hostname);
-
-        Thread t = new Thread(new NetworkManager(client));
-        t.start();
-
-        // System.out.println(ipAddress);
-
-        while (!client.isClosed()) {
-            byte[] sendData = new byte[1024];
-            String line = sc.nextLine();
-
-        }
-    }
-
-    /**
-     * Reads the file and sets up the routes table
-     */
-    public void start() {
+    public static void main(String[] args) throws IOException {
+        // Reads the file and sets up the routes table
         String[] defaultRoutes;
 
         defaultRoutes = io.getDefaultRoutes();
@@ -60,6 +31,90 @@ public class NetworkManager implements Runnable {
                     .ip(routeIp)
                     .port(ConfigurationConstants.DEFAULT_PORT)
                     .weight(ConfigurationConstants.DEFAULT_WEIGHT));
+        }
+
+        // Inicializacao do scanner
+        sc = new Scanner(System.in);
+
+        // Inicializacao da socket
+        client = new DatagramSocket(9000, InetAddress.getByName("192.168.0.24"));
+        System.out.println(client.getInetAddress());
+
+        // IP da maquina
+        InetAddress ipAddress = client.getInetAddress();
+        // System.out.println(ipAddress);
+
+        // Mensagem quando o roteador se conectar na rede
+        byte[] sendData = new byte[1024];
+        String msg = "*" + ipAddress.toString();
+        sendData = msg.getBytes();
+        for (Route r : routes) {
+            InetAddress ipReceiver = InetAddress.getByName(r.getIp());
+            int portReceiver = r.getPort();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipReceiver, portReceiver);
+            client.send(sendPacket);
+        }
+
+        // Criacao das threads
+        Thread t1 = new Thread(new NetworkManager()::routeAnnouncer);
+        Thread t2 = new Thread(new NetworkManager()::messageReader);
+
+        // Inicializacao das threads
+        t1.start();
+        t2.start();
+
+        messageSender();
+    }
+
+    // Envio da tabela de roteamento
+    public void routeAnnouncer() {
+        System.out.println("AQUI 1");
+        while (true) {
+            // try {
+            // // Espera de 15 segundos para envio da tabela de roteamento
+            // wait(15000);
+
+            // // Transformando a tabela de roteamento em mensagem
+            // String msg = "";
+            // for (Route r : routes) {
+            // msg += "@" + r.getIp() + "-" + r.getWeight();
+            // }
+
+            // // Montando o pacote
+            // byte[] sendData = new byte[1024];
+            // sendData = msg.getBytes();
+            // DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length);
+
+            // // Envio do pacote
+            // client.send(sendPacket);
+            // } catch (InterruptedException e) {
+            // e.printStackTrace();
+            // } catch (IOException e) {
+            // e.printStackTrace();
+            // }
+        }
+    }
+
+    // Leitor de mensagens
+    public void messageReader() {
+        System.out.println("AQUI 2");
+        while (true) {
+            // try {
+            // // Recebimento da mensagem
+            // byte[] receiveData = new byte[1024];
+            // DatagramPacket receivePacket = new DatagramPacket(receiveData,
+            // receiveData.length);
+            // client.receive(receivePacket);
+            // } catch (IOException e) {
+            // e.printStackTrace();
+            // }
+        }
+    }
+
+    // Enviador de mensagens
+    public static void messageSender() {
+        while (true) {
+
         }
     }
 
@@ -86,7 +141,5 @@ public class NetworkManager implements Runnable {
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
     }
 }
